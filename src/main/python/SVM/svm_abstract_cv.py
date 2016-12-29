1c01808eebb35faac4407ada06259f4d83239310
claudiusalp <alptekin.orbay@gmail.com>
2016-12-29 23:24:41 +0300

    Scprits are designed to run on my local computer.

--- src/main/python/SVM/svm_abstract_cv.py
+++ src/main/python/SVM/svm_abstract_cv.py
@@ -1,6 +1,6 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 
-import svm_core as sc
+import src.main.python.SVM.svm_core as sc
 import os
 import numpy as np
 from sklearn import cross_validation
@@ -8,53 +8,39 @@ import argparse
 import math
 import sys
 
-script_dir_path = os.path.join(os.path.realpath(__file__),"..")
+script_dir_path = os.path.join(os.path.realpath(__file__), "..")
 minter_root = os.path.abspath(os.path.join(script_dir_path, "../../../.."))
-sys.path.append(minter_root+"/lib")
+sys.path.append(minter_root + "/lib")
 
-from modules import paperparse as pp
+from lib.modules import paperparse as pp
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument( "target", help ="target directory containing annotated .sp files")
-	parser.add_argument( "-p", "--probas", help ="Proabibility value cutoff (optional)", type = float, default = None)
-	args = parser.parse_args()
-	baseFolder = args.target
-	
-	"""
-	Training of the SVM
-	"""
-
-	#Folder Setup
-	
-	baseFolder = "tests/svm_trainer/"
-	coreTrain = baseFolder + "Core/lactobacillus_acidophilus#escherichia_coli.ann"
-	trainFile = baseFolder + "collated_train.ann"
-	files = sc.read(trainFile)
-	abstracts = [i[2] for i in files]
-	targets = np.array([True if i[0].strip() == ">T" else False for i in files ])
-
-	skf = cross_validation.StratifiedKFold(y = targets, n_folds = 10, shuffle = True)
-
-	scoring = []
-
-	print("DATA SET LENGTH: ", len(targets))
-	for train_index, test_index in skf:
-		# print(train_index)
-		# print(test_index)
-
-		#Training pairs: all pairs containing annotated true-true interactions
-		training_set = [abstracts[i] for i in train_index]
-		training_set_targets = [targets[i] for i in train_index]
-		testing_set = [abstracts[i] for i in test_index]
-		testing_set_targets = [targets[i] for i in test_index]
-		svm = sc.make_classifier(C=1)
-		svm.fit(training_set, training_set_targets)
-
-		scoring.append(sc.grade(svm, testing_set, testing_set_targets, probas = args.probas))
-
-	
-
-	print("-------__RESULTS_____---------")
-	for i in scoring:
-		print(i)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("target", help="target directory containing annotated .sp files")
+    parser.add_argument("-p", "--probas", help="Proabibility value cutoff (optional)", type=float, default=None)
+    args = parser.parse_args()
+    baseFolder = args.target
+    ### Tranining og the SVM
+    #baseFolder = "tests/svm_trainer/"
+    coreTrain = baseFolder + "Core/lactobacillus_acidophilus#escherichia_coli.ann"
+    trainFile = baseFolder + "collated_train.ann"
+    files = sc.read(trainFile)
+    abstracts = [i[2] for i in files]
+    targets = np.array([True if i[0].strip() == ">T" else False for i in files])
+    skf = cross_validation.StratifiedKFold(y=targets, n_folds=10, shuffle=True)
+    scoring = []
+    print("DATA SET LENGTH: ", len(targets))
+    for train_index, test_index in skf:
+        # print(train_index)
+        # print(test_index)
+        # Training pairs: all pairs containing annotated true-true interactions
+        training_set = [abstracts[i] for i in train_index]
+        training_set_targets = [targets[i] for i in train_index]
+        testing_set = [abstracts[i] for i in test_index]
+        testing_set_targets = [targets[i] for i in test_index]
+        svm = sc.make_classifier(C=1)
+        svm.fit(training_set, training_set_targets)
+        scoring.append(sc.grade(svm, testing_set, testing_set_targets, probas=args.probas))
+    print("-------__RESULTS_____---------")
+    for i in scoring:
+        print(i)
--- src/main/python/SVM/svm_scanner.py
+++ src/main/python/SVM/svm_scanner.py
@@ -10,16 +10,18 @@ from math import log
 import argparse
 import sys
 
-script_dir_path = os.path.join(os.path.realpath(__file__),"..")
+script_dir_path = os.path.join(os.path.realpath(__file__), "..")
 minter_root = os.path.abspath(os.path.join(script_dir_path, "../../../.."))
-sys.path.append(minter_root+"/lib")
-from modules import paperparse as pp
+sys.path.append(minter_root + "/lib")
+from lib.modules import paperparse as pp
 
 parser = argparse.ArgumentParser()
-parser.add_argument( "SVM", help ="pickled SVM file")
-parser.add_argument( "target", help = "target directory containing spFiles to be scanned")
-parser.add_argument( "-o", "--output", help ="output directory", default = os.path.join(minter_root, "output", "svm_scanner"))
-parser.add_argument( "-j", "--json", help = "Use json files instead", action = "store_true", default = 1) #change to sp switch
+parser.add_argument("SVM", help="pickled SVM file")
+parser.add_argument("target", help="target directory containing spFiles to be scanned")
+parser.add_argument("-o", "--output", help="output directory",
+                    default=os.path.join(minter_root, "output", "svm_scanner"))
+parser.add_argument("-j", "--json", help="Use json files instead", action="store_true",
+                    default=1)  # change to sp switch
 args = parser.parse_args()
 
 print("LOADING SVM: ", args.SVM)
@@ -29,7 +31,7 @@ target = args.target
 outdir = args.output
 
 if not os.path.exists(outdir):
-	os.makedirs(outdir)
+    os.makedirs(outdir)
 targetPaths = [os.path.join(target, i) for i in os.listdir(target)]
 
 cur = 0
@@ -38,47 +40,47 @@ lower_bound = 0
 total_hits = 0
 
 for file_num, targetPath in enumerate(targetPaths):
-	print("FILE NUMBER: ", file_num)
-	if args.json:
-		pair = pp.SpFile(targetPath, purge = True)
-	else:
-		pair = pp.spFile(targetPath, purge = True)
+    print("FILE NUMBER: ", file_num)
+    if args.json:
+        pair = pp.SpFile(targetPath, purge=True)
+    else:
+        pair = pp.spFile(targetPath, purge=True)
 
-	hits = 0
+    hits = 0
 
-	total = len(targetPaths)
-	print ("analysing pair " +str(cur) + "/" + str(total))
-	print(targetPath)
-	cur+=1
+    total = len(targetPaths)
+    print("analysing pair " + str(cur) + "/" + str(total))
+    print(targetPath)
+    cur += 1
 
-	if args.json:
-		temp = pp.SpFile(targetPath, reduced = True)
-	else:
-		temp = pp.spFile(targetPath, reduced = True)
-	if temp.summary["NEG"] != "1":
-		lower_bound += len(pair.papers)
-		# print(pair.papers)
-		# print(len(pair.papers))
-	total_papers += len(pair.papers)
+    if args.json:
+        temp = pp.SpFile(targetPath, reduced=True)
+    else:
+        temp = pp.spFile(targetPath, reduced=True)
+    if temp.summary["NEG"] != "1":
+        lower_bound += len(pair.papers)
+    # print(pair.papers)
+    # print(len(pair.papers))
+    total_papers += len(pair.papers)
 
-	paper_count = len(pair.papers)
-	for paper_num, paper in enumerate(pair.papers):	
-		print("processing paper {}/{}".format(paper_num, paper_count))
-		#implement temporary lowers to allow persistance of the original paper
-		if paper["AB"]:
-			if svm.predict([paper["AB"]]):
-				paper["ABHT"] = "1"
-				hits += 1
-				pair.summary["INT"] = "1"
-				pair.summary["NEG"] = "1"
-				print("HIT")
-				total_hits +=1
-		else:
-			paper["ABHT"] = "0"
-			pair.summary["INT"] = "0"
-			pair.summary["NEG"] = "0"
+    paper_count = len(pair.papers)
+    for paper_num, paper in enumerate(pair.papers):
+        print("processing paper {}/{}".format(paper_num, paper_count))
+        # implement temporary lowers to allow persistance of the original paper
+        if paper["AB"]:
+            if svm.predict([paper["AB"]]):
+                paper["ABHT"] = "1"
+                hits += 1
+                pair.summary["INT"] = "1"
+                pair.summary["NEG"] = "1"
+                print("HIT")
+                total_hits += 1
+        else:
+            paper["ABHT"] = "0"
+            pair.summary["INT"] = "0"
+            pair.summary["NEG"] = "0"
 
-	pair.writeSpFileHits(os.path.join(outdir, os.path.basename(targetPath)))
+    pair.writeSpFileHits(os.path.join(outdir, os.path.basename(targetPath)))
 
 print("total_papers =", total_papers)
 print("lower_bound =", lower_bound)
